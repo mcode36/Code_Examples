3a85d4ddedf572e2499a4a2d71f41c752c4cf8ac
mcode36 <48412323+mcode36@users.noreply.github.com>
2020-07-30 14:28:19 -0700

    Update breakout.py

--- Python/pygame/breakout.py
+++ Python/pygame/breakout.py
@@ -10,8 +10,15 @@ import pygame
 import sys
 pygame.init()
 
+# Setup colors
+red = (255,0,0)
+blue = (0,0,255)
+black = (0,0,0)
+
 # Sound Effect
 hit = pygame.mixer.Sound('sms-alert-1-daniel_simon.wav')
+
+
 
 # Setup the main display
 display_width = 600
@@ -21,6 +28,13 @@ display = pygame.display.set_mode(display_res)
 display_color = (0, 0, 0)
 display.fill(display_color)
 
+def message_display(text):
+    largeText = pygame.font.Font('freesansbold.ttf',90)
+    TextSurf = largeText.render(text, True, red)
+    TextRect = TextSurf.get_rect()
+    TextRect.center = (int(display_width/2),int(display_height/2))
+    display.blit(TextSurf, TextRect)
+
 # Setup the player controlled rectangle
 player_width = 100
 player_height = 20
@@ -29,14 +43,16 @@ player_surface = pygame.Surface(player_size)
 player_rect = player_surface.get_rect()
 rect_speed = 1
 player_rect.x = 300
-player_rect.y = display_height-50
+player_rect.y = display_height-60
 player_color = (255, 255, 255)
 player_surface.fill(player_color)
 
 # Setup the bouncing ball
 ball = pygame.image.load("PngItem_25193s.png")
+# Image size: 40 x 40
 ball_rect = ball.get_rect()
-ball_rect.y = 400
+ball_rect.x = 300
+ball_rect.y = player_rect.y - 50
 ball_speed = [1, 1]
 
 # Setup the target blocks
@@ -61,6 +77,7 @@ for target_col in range(int(display_width / (target_width+x_offs))):
         target_surf_list.append(target_surf)
         target_rect_list.append(target_rect)
 
+game_status = 'start'
 while 1:
     ## Adjust FPS
     clock = pygame.time.Clock()
@@ -69,17 +86,23 @@ while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_a]:
+    if keys[pygame.K_LEFT]:
         player_rect = player_rect.move([-rect_speed, 0])
-    if keys[pygame.K_d]:
+    if keys[pygame.K_RIGHT]:
         player_rect = player_rect.move([rect_speed, 0])
+    if keys[pygame.K_q]:
+        break
 
     ball_rect = ball_rect.move(ball_speed)
     if ball_rect.left < 0 or ball_rect.right > display_width:
         ball_speed[0] = -ball_speed[0]
-    if ball_rect.top < 0 or ball_rect.bottom > display_height:
+    if ball_rect.top < 0 :
         ball_speed[1] = -ball_speed[1]
+    if ball_rect.bottom > display_height:
+        ball_speed = [0, 0]
+        game_status = 'over'
 
     if player_rect.colliderect(ball_rect):
         hit.play()  
@@ -90,9 +113,16 @@ while 1:
             del target_rect_list[idx]
             del target_surf_list[idx]
             ball_speed[1] = -ball_speed[1]
+
+    # Try text
+    
     display.fill(display_color)
     display.blit(player_surface, player_rect)
     display.blit(ball, ball_rect)
     for x in range(len(target_surf_list)):
         display.blit(target_surf_list[x], target_rect_list[x])
+    if game_status == 'over':
+        message_display('Game Over')
     pygame.display.flip()
+
+print("Bye")
